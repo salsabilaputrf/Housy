9da3e59a5697fa33cca6684f649a118c088ad0c5
salsabilaputrf <akun07378@gmail.com>
2023-02-14 00:44:26 +0700

    display image card

--- client/src/components/CardProperties.jsx
+++ client/src/components/CardProperties.jsx
@@ -10,7 +10,6 @@ import { toCurrency } from "lib/Currency";
 import css from "./CardProperties.module.css";
 
 export default function CardProperties(props) {
-
 	const [state, dispatch] = useContext(AppContext);
 
 	// if (props.searchCity === true){
@@ -23,83 +22,79 @@ export default function CardProperties(props) {
 	});
 
 	let properties = state.city;
-	console.log("city", properties)
-	if (properties === undefined){
-		if (state.filter === undefined){
+	console.log("city", properties);
+	if (properties === undefined) {
+		if (state.filter === undefined) {
 			properties = allProperty;
-			console.log("all", properties)
-		}else{
+			console.log("all", properties);
+		} else {
 			properties = state.filter;
-			console.log("filter", properties)
+			console.log("filter", properties);
 		}
 	}
 
 	console.log("data showed", properties);
 	return (
 		<>
-		{properties === undefined || properties.length === 0 ? (
-			<div
-				className='d-flex align-items-center justify-content-center'
-				style={{ minHeight: "90vh"}}
-			>
-				<div className='text-center bg-white rounded-4 p-5 shadow'>
-					<h2>Property Tidak Ditemukan</h2>
-					{/* <p>Silahkan lakukan checkin terlebih dahulu</p>
+			{properties === undefined || properties.length === 0 ? (
+				<div
+					className='d-flex align-items-center justify-content-center'
+					style={{ minHeight: "90vh" }}
+				>
+					<div className='text-center bg-white rounded-4 p-5 shadow'>
+						<h2>Property Tidak Ditemukan</h2>
+						{/* <p>Silahkan lakukan checkin terlebih dahulu</p>
 					<Link to='/' className='btn btn-primary px-4 py-2 mt-2'>
 						Kembali
 					</Link> */}
+					</div>
 				</div>
-			</div>
-		) : (
-			<>
-			{properties?.map((room, k) => {
-				return (
-					<Link
-					
-						to={"/detail/" + room.id}
-						key={k}
-						className='w-100'
-						style={{ textDecoration: "none" }}
-					>
-						<Card className={props.className}>
-							<div className='position-absolute mt-3 ms-3 d-flex gap-2'>
-								{room.amenities.map((amenity, k) => (
-									<span key={k} className={css.Tag}>
-										{amenity}
-									</span>
-								))}
-							</div>
-							<div className={css.WrapperPrimaryImage}>
-								<Image
-									variant='top'
-									className={css.PrimaryImage}
-									src={
-										// "https://3408-2404-8000-1004-b94f-71a6-be6-bec4-1ca1.ap.ngrok.io/uploads/"
-										"http://localhost:5000/uploads/" + room.image
-									}
-								/>
-							</div>
-							<Card.Body>
-								<Card.Title>
-									<strong>
-										{toCurrency(room.price)} / {room.type_rent}
-									</strong>
-								</Card.Title>
-								<Card.Subtitle className='mb-2'>
-									{room.bedroom} Beds, {room.bathroom} Bath, {room.size} Sqft
-								</Card.Subtitle>
-								<Card.Text>
-									{room.district}, {room.city.name}
-								</Card.Text>
-								{/* <Button variant="primary">Go somewhere</Button> */}
-							</Card.Body>
-						</Card>
-					</Link>
-				);
-				
-			})}
-			</>
-		)}
+			) : (
+				<>
+					{properties?.map((room, k) => {
+						return (
+							<Link
+								to={"/detail/" + room.id}
+								key={k}
+								className='w-100'
+								style={{ textDecoration: "none" }}
+							>
+								<Card className={props.className}>
+									<div className='position-absolute mt-3 ms-3 d-flex gap-2'>
+										{room.amenities.map((amenity, k) => (
+											<span key={k} className={css.Tag}>
+												{amenity}
+											</span>
+										))}
+									</div>
+									<div className={css.WrapperPrimaryImage}>
+										<Image
+											variant='top'
+											className={css.PrimaryImage}
+											src={room.image}
+										/>
+									</div>
+									<Card.Body>
+										<Card.Title>
+											<strong>
+												{toCurrency(room.price)} / {room.type_rent}
+											</strong>
+										</Card.Title>
+										<Card.Subtitle className='mb-2'>
+											{room.bedroom} Beds, {room.bathroom} Bath, {room.size}{" "}
+											Sqft
+										</Card.Subtitle>
+										<Card.Text>
+											{room.district}, {room.city.name}
+										</Card.Text>
+										{/* <Button variant="primary">Go somewhere</Button> */}
+									</Card.Body>
+								</Card>
+							</Link>
+						);
+					})}
+				</>
+			)}
 		</>
 	);
 }
--- client/src/pages/Tenant/Booking.jsx
+++ client/src/pages/Tenant/Booking.jsx
@@ -30,7 +30,6 @@ export default function MyBooking() {
 	// 	return response.data.data;
 	// });
 
-	
 	// console.log("midtrans", midtrans)
 
 	useEffect(() => {
@@ -38,80 +37,82 @@ export default function MyBooking() {
 		const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
 		//change this according to your client-key
 		const myMidtransClientKey = "SB-Mid-client-MxZZnkEzX-K3qdGi";
-	
+
 		let scriptTag = document.createElement("script");
 		scriptTag.src = midtransScriptUrl;
 		// optional if you want to set script attribute
 		// for example snap.js have data-client-key attribute
 		scriptTag.setAttribute("data-client-key", myMidtransClientKey);
-	
+
 		document.body.appendChild(scriptTag);
 		return () => {
-		  document.body.removeChild(scriptTag);
+			document.body.removeChild(scriptTag);
 		};
-	  }, []);
+	}, []);
 
-	  const handlePay = useMutation(async () => {
+	const handlePay = useMutation(async () => {
 		try {
-		  // Get data from transaction
-		//   const data = {
-		// 	property_id: transaction?.property_id,
-		// 	checkin: moment(transaction?.checkin).format('YYYY-MM-DD'),
-		// 	checkout: moment(transaction?.checkout).format('YYYY-MM-DD'),
-		// 	status: "pending",
-		// 	total: transaction?.total,
-		// };
-	
-		//   // Data body
-		//   const body = JSON.stringify(data);
-	
-		  //Configuration
-		  const config = {
-			// method: "PATCH",
-			headers: {
-			  Authorization: "Basic " + localStorage.token,
-			  "Content-type": "application/json",
-			},
-		  };
-	
-		  // Insert transaction data
-		  const response = await API.get("/createMidtrans/" + transaction?.id, config);
-		  console.log("midtrans response",response);
-		  const token = response.data.data.token;
-		  console.log("token", token)
+			// Get data from transaction
+			//   const data = {
+			// 	property_id: transaction?.property_id,
+			// 	checkin: moment(transaction?.checkin).format('YYYY-MM-DD'),
+			// 	checkout: moment(transaction?.checkout).format('YYYY-MM-DD'),
+			// 	status: "pending",
+			// 	total: transaction?.total,
+			// };
 
-		  window.snap.pay(token, {
-			onSuccess: function (result) {
-			  /* You may add your own implementation here */
-			  console.log(result);
-			  alert("Payment success");
-			},
-			onPending: function (result) {
-			  /* You may add your own implementation here */
-			  console.log(result);
-			  alert("Payment pending");
-			},
-			onError: function (result) {
-			  /* You may add your own implementation here */
-			  console.log(result);
-			  alert("Payment error");
-			},
-			onClose: function () {
-			  /* You may add your own implementation here */
-			  alert("you closed the popup without finishing the payment");
-			},
-		  });
+			//   // Data body
+			//   const body = JSON.stringify(data);
+
+			//Configuration
+			const config = {
+				// method: "PATCH",
+				headers: {
+					Authorization: "Basic " + localStorage.token,
+					"Content-type": "application/json",
+				},
+			};
+
+			// Insert transaction data
+			const response = await API.get(
+				"/createMidtrans/" + transaction?.id,
+				config
+			);
+			console.log("midtrans response", response);
+			const token = response.data.data.token;
+			console.log("token", token);
+
+			window.snap.pay(token, {
+				onSuccess: function (result) {
+					/* You may add your own implementation here */
+					console.log(result);
+					alert("Payment success");
+				},
+				onPending: function (result) {
+					/* You may add your own implementation here */
+					console.log(result);
+					alert("Payment pending");
+				},
+				onError: function (result) {
+					/* You may add your own implementation here */
+					console.log(result);
+					alert("Payment error");
+				},
+				onClose: function () {
+					/* You may add your own implementation here */
+					alert("you closed the popup without finishing the payment");
+				},
+			});
 		} catch (error) {
-		  console.log(error);
+			console.log(error);
 		}
-	  });
-
+	});
 
 	return (
 		<Layout className='bg-tertiary'>
 			<div className=''>
 				<div className={css.MaxWidth}>
-					{ transaction === undefined ? (
+					{transaction === undefined ? (
 						<div
 							className='d-flex align-items-center justify-content-center'
 							style={{ minHeight: "90vh" }}
@@ -123,136 +124,150 @@ export default function MyBooking() {
 									Kembali
 								</Link>
 							</div>
-						</div>  
+						</div>
 					) : (
 						<>
-						<div className={css.Card}>
-							<div className='d-flex justify-content-between'>
-								<div className={css.CardLeft}>
-									<Image src={logo} alt='Logo' className={css.ImgLogo} />
-									<div className='d-flex gap-3 align-items-center'>
-										<div className='pe-4'>
-											<h2>{transaction?.property.name}</h2>
-											<p style={{ width: "19.5rem" }}>
-												{transaction?.property.address}, <br></br>{transaction?.property.district}
-												, {transaction?.property.city.name}
-											</p>
-											<span className={css.Badge} style={{textTransform: "capitalize"}}>{transaction?.status}</span>
-										</div>
-										<div
-											style={{ width: "14rem" }}
-											className='d-flex align-items-center gap-4'
-										>
+							<div className={css.Card}>
+								<div className='d-flex justify-content-between'>
+									<div className={css.CardLeft}>
+										<Image src={logo} alt='Logo' className={css.ImgLogo} />
+										<div className='d-flex gap-3 align-items-center'>
+											<div className='pe-4'>
+												<h2>{transaction?.property.name}</h2>
+												<p style={{ width: "19.5rem" }}>
+													{transaction?.property.address}, <br></br>
+													{transaction?.property.district},{" "}
+													{transaction?.property.city.name}
+												</p>
+												<span
+													className={css.Badge}
+													style={{ textTransform: "capitalize" }}
+												>
+													{transaction?.status}
+												</span>
+											</div>
+											<div
+												style={{ width: "14rem" }}
+												className='d-flex align-items-center gap-4'
+											>
+												<div className=''>
+													<Image src={Stepper} width={16} />
+												</div>
+												<div className='d-flex flex-column gap-4'>
+													<div>
+														<strong className='d-block'>Checkin</strong>
+														<span className='text-secondary'>
+															{moment(transaction?.checkin).format(
+																"DD MMMM YYYY"
+															)}
+														</span>
+													</div>
+													<div>
+														<strong className='d-block'>Checkout</strong>
+														<span className='text-secondary'>
+															{moment(transaction?.checkout).format(
+																"DD MMMM YYYY"
+															)}
+														</span>
+													</div>
+												</div>
+											</div>
 											<div className=''>
-												<Image src={Stepper} width={16} />
-											</div>
-											<div className='d-flex flex-column gap-4'>
 												<div>
-													<strong className='d-block'>Checkin</strong>
-													<span className='text-secondary'>
-														{moment(transaction?.checkin).format("DD MMMM YYYY")}
-													</span>
+													<strong className='d-block'>Amenities</strong>
+													<ul>
+														{transaction?.property.amenities.map((x, k) => {
+															return (
+																<li key={k} className='text-secondary'>
+																	{x}
+																</li>
+															);
+														})}
+													</ul>
 												</div>
 												<div>
-													<strong className='d-block'>Checkout</strong>
-													<span className='text-secondary'>
-														{moment(transaction?.checkout).format("DD MMMM YYYY")}
+													<strong className='d-block'>Type of rent</strong>
+													<span className='text-secondary ps-4'>
+														{transaction?.property.type_rent}
 													</span>
 												</div>
-											</div>
-										</div>
-										<div className=''>
-											<div>
-												<strong className='d-block'>Amenities</strong>
-												<ul>
-													{transaction?.property.amenities.map((x, k) => {
-														return (
-															<li key={k} className='text-secondary'>
-																{x}
-															</li>
-														);
-													})}
-												</ul>
-											</div>
-											<div>
-												<strong className='d-block'>Type of rent</strong>
-												<span className='text-secondary ps-4'>
-													{transaction?.property.type_rent}
-												</span>
 											</div>
 										</div>
 									</div>
-								</div>
-								<div className={css.CardRight}>
-									<div>
-										<h1 className='fw-bold'>Booking</h1>
+									<div className={css.CardRight}>
+										<div>
+											<h1 className='fw-bold'>Booking</h1>
 
-										<p>
-											<strong>{moment(transaction?.checkin).format("dddd")}</strong>,{" "}
-											{moment(transaction?.checkin).format("DD MMMM YYYY")}
-										</p>
+											<p>
+												<strong>
+													{moment(transaction?.checkin).format("dddd")}
+												</strong>
+												, {moment(transaction?.checkin).format("DD MMMM YYYY")}
+											</p>
+										</div>
+										<div className={css.WrapperCardImage}>
+											<Image
+												className={css.CardImage}
+												src={
+													process.env.PUBLIC_URL + "/img/Uploads/receipt.png"
+												}
+											/>
+										</div>
+										<small className='text-secondary'>
+											Upload payment proof
+										</small>
 									</div>
-									<div className={css.WrapperCardImage}>
-										<Image
-											className={css.CardImage}
-											src={process.env.PUBLIC_URL + "/img/Uploads/receipt.png"}
-										/>
-									</div>
-									<small className='text-secondary'>Upload payment proof</small>
+								</div>
+								<div className=''>
+									<Table className='mt-4 mb-5'>
+										<thead>
+											<tr>
+												<th>No</th>
+												<th>Full Name</th>
+												<th>Gender</th>
+												<th>Phone</th>
+												<th></th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr className='text-secondary'>
+												<td>1</td>
+												<td>{transaction?.user.fullname}</td>
+												<td>{transaction?.user.gender}</td>
+												<td>{transaction?.user.phone}</td>
+												<td className='fw-semibold text-black'>
+													Long Time Rent : 1 {transaction?.property.type_rent}
+												</td>
+											</tr>
+											<tr>
+												<td colSpan='4'></td>
+												<td className='fw-semibold' style={{ width: "18rem" }}>
+													total <span style={{ padding: "0 2.45rem" }}></span> :{" "}
+													<span className='text-danger'>
+														Rp. {transaction?.property.price}
+													</span>
+												</td>
+											</tr>
+										</tbody>
+									</Table>
 								</div>
 							</div>
-							<div className=''>
-								<Table className='mt-4 mb-5'>
-									<thead>
-										<tr>
-											<th>No</th>
-											<th>Full Name</th>
-											<th>Gender</th>
-											<th>Phone</th>
-											<th></th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr className='text-secondary'>
-											<td>1</td>
-											<td>{transaction?.user.fullname}</td>
-											<td>{transaction?.user.gender}</td>
-											<td>{transaction?.user.phone}</td>
-											<td className='fw-semibold text-black'>
-												Long Time Rent : 1 {transaction?.property.type_rent}
-											</td>
-										</tr>
-										<tr>
-											<td colSpan='4'></td>
-											<td className='fw-semibold' style={{ width: "18rem" }}>
-												total <span style={{ padding: "0 2.45rem" }}></span> :{" "}
-												<span className='text-danger'>
-													Rp. {transaction?.property.price}
-												</span>
-											</td>
-										</tr>
-									</tbody>
-								</Table>
+							<div className='mt-4 me-5'>
+								<div className='d-flex justify-content-end'>
+									<Button
+										type='button'
+										onClick={() => handlePay.mutate()}
+										className={"btn btn-primary fw-bold fs-5 ms-auto"}
+										style={{ padding: "1rem 6rem" }}
+									>
+										PAY
+									</Button>
+								</div>
 							</div>
-							
-						</div>
-						<div className='mt-4 me-5'>
-							<div className='d-flex justify-content-end'>
-								<Button
-									type='button'
-									onClick={() => handlePay.mutate()}
-									className={"btn btn-primary fw-bold fs-5 ms-auto"}
-									style={{ padding: "1rem 6rem" }}
-								>
-									PAY
-								</Button>
-							</div>
-						</div>
 						</>
-
 					)}
 				</div>
 			</div>
-		</Layout>	
+		</Layout>
 	);
 }
